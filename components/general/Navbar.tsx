dc9e504aaa1c9a67c9a19ae0c81a7b2b940abd6c
MD Mehedi Hashan Molla <mdmehedihashanmolla@gmail.com>
2025-03-05 17:31:23 +0530

    init

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/logo.png";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./ThemeToggle";
+import { auth, signOut } from "@/app/utils/auth";
 
-export function Navbar() {
+export async function Navbar() {
+  const session = await auth();
+
   return (
     <nav className="flex items-center justify-between py-5">
       <Link href="/" className="flex items-center gap-2">
@@ -14,8 +17,24 @@ export function Navbar() {
         </h1>
       </Link>
       <div className="flex items-center gap-4">
-        <ThemeToggle/>
-        <Button className="bg-purple-500">Login</Button>
+        <ThemeToggle />
+        {session?.user ? (
+          <form
+            action={async () => {
+              "use server";
+              await signOut({ redirectTo: "/" });
+            }}
+          >
+            <Button className="bg-purple-500">Logout</Button>
+          </form>
+        ) : (
+          <Link
+            href="/login"
+            className={buttonVariants({ variant: "outline", size: "lg" })}
+          >
+            Login
+          </Link>
+        )}
       </div>
     </nav>
   );
--- components/general/SubmitButton.tsx
+++ components/general/SubmitButton.tsx
@@ -23,7 +23,7 @@ export function GeneralSubmitButton({ text,variant,width,icon }: GeneralSubmitBu
   const { pending } = useFormStatus();
 
   return (
-    <Button variant={variant} className={width}>
+    <Button variant={variant} className={width} disabled={pending}>
       {pending ? (
         <>
           <Loader2 className="size-4 animate-spin" />
