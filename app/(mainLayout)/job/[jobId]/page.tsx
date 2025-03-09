304593432086f4f931f4d7e4a91eb27b60587b2f
MD Mehedi Hashan Molla <mdmehedihashanmolla@gmail.com>
2025-03-09 12:00:47 +0530

    init

--- app/(mainLayout)/job/[jobId]/page.tsx
+++ app/(mainLayout)/job/[jobId]/page.tsx
@@ -4,6 +4,8 @@ import { benefits } from "@/app/utils/listOfBenefits";
 import { JsonToHtml } from "@/components/general/JsonToHtml";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { Heart } from "lucide-react";
 import { notFound } from "next/navigation";
 
@@ -20,6 +22,7 @@ async function getJob(jobId: string) {
       employmentType: true,
       benefits: true,
       createdAt: true,
+      listingDuration: true,
       company: {
         select: {
           name: true,
@@ -41,8 +44,8 @@ export default async function JobIdPage({ params }: { params: Params }) {
   const data = await getJob(jobId);
   const locationFlag = getFlagEmoji(data.location);
   return (
-    <div className="grid lg:grid-cols-[1fr, 400px] gap-8">
-      <div className="space-y-8">
+    <div className="grid lg:grid-cols-3 gap-8">
+      <div className="space-y-8 col-span-2">
         <div className="flex items-center justify-between">
           <div>
             <h1 className="text-3xl font-bold">Marketing Manager</h1>
@@ -73,22 +76,65 @@ export default async function JobIdPage({ params }: { params: Params }) {
         </section>
         <section>
           <h3 className="font-semibold mb-4">Benifits</h3>
+
           <div className="flex flex-wrap gap-3">
             {benefits.map((benefit) => {
-                const isOffered = data.benefits.includes(benefit.id);
-              return  <Badge
-              key={benefit.id}
-              variant={isOffered ? "default" : "outline"}
-              className="cursor-pointer transition-all hover:scale-105 active:scale-95 select-none text-sm px-4 py-1.5 rounded-full"
-            >
-              <span className="flex items-center gap-2">
-                {benefit.icon}
-                {benefit.label}
-              </span>
-            </Badge>;
+              const isOffered = data.benefits.includes(benefit.id);
+              return (
+                <Badge
+                  className={cn(
+                    isOffered
+                      ? "bg-purple-500"
+                      : "opacity-75 cursor-not-allowed text-sm px-4 py-1.5 rounded-full"
+                  )}
+                  key={benefit.id}
+                  variant={isOffered ? "default" : "outline"}
+                >
+                  <span className="flex items-center gap-2">
+                    {benefit.icon}
+                    {benefit.label}
+                  </span>
+                </Badge>
+              );
             })}
           </div>
         </section>
+      </div>
+      <div className="space-y-6">
+        <Card className="p-6">
+          <div className="space-y-4">
+            <div>
+              <h3 className="font-semibold">Apply now</h3>
+              <p className="text-muted-foreground text-sm mt-1">
+                Please let {data.company.name}know you found this job on
+                DevRecruit. This helps us grow!
+              </p>
+            </div>
+            <Button className="w-full bg-purple-500">Apply now</Button>
+          </div>
+        </Card>
+        {/* Job Details Card */}
+
+        <Card className="p-6">
+          <h3 className="font-semibold">About the job</h3>
+          <div className="space-y-2">
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">
+                Apply before
+              </span>
+              <span className="text-sm">
+                {new Date(
+                  data.createdAt.getTime() +
+                    data.listingDuration * 24 * 60 * 60 * 1000
+                ).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
